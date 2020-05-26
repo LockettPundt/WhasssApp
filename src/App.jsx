86379df228e6039750717d06b8172e21211e2c4c
Lockett Pundt <kalifornium@hotmail.com>
2020-05-26 11:18:07 -0400

    initial clean up/set up.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hi. this is the start of my electron app.
+    </div>
+  );
+}
+
+export default App;
