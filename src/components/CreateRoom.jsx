d089b9c7475384db18bba047cc47d3d679ba0bbc
Lockett Pundt <kalifornium@hotmail.com>
2020-06-10 13:07:18 -0400

    created an object for the submitted user info in teh handleSumbit function. that is passed into a post request with axios to the server to get a response as to whether the name is free or not. if so, the room is created and the user is redirected to the room, if not the user is prompted an error message in the placeholder text in the room name. I will implement a password length and type.

--- src/components/CreateRoom.jsx
+++ src/components/CreateRoom.jsx
@@ -1,22 +1,44 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import axios from 'axios';
+import API_URL from '../utils/apiConn';
 
 const CreateRoom = () => {
-  const [createName, setRoomName] = useState('');
+  const [roomName, setRoomName] = useState('');
+  const [roomNamePlaceholder, setRoomNamePlaceholder] = useState('Enter a Room Name');
   const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
   const history = useHistory();
 
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const chatRoomInfo = {
+      chatRoomName: roomName,
+      password,
+      dateCreated: new Date(),
+      lastAccess: new Date(),
+    };
+
     // db validate name. push if free.
-    // update store of room name.
-    // redirect to chatroom.
-    localStorage.setItem('userName', userName);
-    localStorage.setItem('chatRoomName', createName);
-    console.log(`/room/${createName}`);
-    history.push(`/room/${createName}`);
+
+    const response = await axios.post(`${API_URL}api/create`, { chatRoomInfo });
+    // console.log(response);
+    if (!response.data.error) {
+      localStorage.setItem('userName', userName);
+      localStorage.setItem('chatRoomName', roomName);
+      console.log(`Connecting to room: ${roomName}`);
+      history.push(`/room/${roomName}`);
+    }
+    if (response.data.error) {
+      const { error } = response.data;
+      if (error.includes('Room')) {
+        setRoomName('');
+        setRoomNamePlaceholder(error);
+      }
+    }
   };
 
 
@@ -25,15 +47,22 @@ const CreateRoom = () => {
       <form onSubmit={(e) => handleSubmit(e)}>
         <input
           type="text"
-          value={createName}
-          placeholder="Enter a Room Name"
+          value={roomName}
+          placeholder={roomNamePlaceholder}
           onChange={(e) => setRoomName(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Enter a password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           required
         />
         <input
           type="text"
           value={userName}
-          placeholder="Enter a Name"
+          placeholder="Enter Your Name"
           onChange={(e) => setUserName(e.target.value)}
           required
         />
