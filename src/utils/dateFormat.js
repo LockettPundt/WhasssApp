8f09be0f62f9c9d40f11618a0dda6c2b63e5c360
Lockett Pundt <kalifornium@hotmail.com>
2020-06-12 13:28:09 -0400

    inital function to format time. the minutes need to display 2 digits if the last one is a zero.

---
+++ src/utils/dateFormat.js
@@ -0,0 +1,14 @@
+
+const dateFormat = (date) => {
+  const dateToFormat = new Date(date);
+  const hour = dateToFormat.getHours() > 12
+    ? dateToFormat.getHours() - 12
+    : dateToFormat.getHours();
+  const minutes = dateToFormat.getMinutes();
+  const AMPM = dateToFormat.getHours() >= 12 ? 'pm' : 'am';
+  const formattedDate = `${hour}:${minutes}${AMPM}`;
+  return formattedDate;
+};
+
+
+export default dateFormat;
